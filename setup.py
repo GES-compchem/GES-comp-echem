ef80ef25fb2f06760ef187bc143223bb7073315d
GES-mpalermo <mattia.palermo@greenenergystorage.eu>
2022-01-04 12:08:17 +0100

    Initial commit
    Added files for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name="GES-comp-echem",
+    version="0.1.0a",
+    description="",
+    long_description="",
+    packages=["compechem"],
+    package_data={'compechem': ['calculators/*',],},
+    install_requires=[],
+)
