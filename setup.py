998264995bee9276ac0483df59a5dd611b629171
GES-lbabetto <luca.babetto@greenenergystorage.eu>
2022-02-18 14:36:59 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="GES-comp-echem",
-    version="0.1.0a",
+    version="0.1.1a",
     description="",
     long_description="",
     packages=["compechem"],
