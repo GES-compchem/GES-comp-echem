bab243d6461055143dacbf3f32993b56c8691da4
GES-lbabetto <luca.babetto@greenenergystorage.eu>
2022-02-24 16:38:28 +0100

    BUGFIX: fixed semicolon instead of equal sign in Ensemble

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="GES-comp-echem",
-    version="0.1.3a",
+    version="0.1.4a",
     description="",
     long_description="",
     packages=["compechem"],
