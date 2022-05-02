ccd79b4416d6895568c5a3011bf8e8ae57b51cac
Luca Babetto <luca.babetto@greenenergystorage.eu>
2022-05-02 18:01:26 +0200

    fixed package_data not being updated for code refactoring

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setuptools
 
 setuptools.setup(
     name="GES-comp-echem",
-    version="0.1.25a",
+    version="0.1.26a",
     description="",
     long_description="",
     packages=["compechem"],
-    package_data={"compechem": ["modules/*",],},
+    package_data={"compechem": ["algorithms/*","calculators/*","functions/*"],},
     install_requires=[],
 )
