f228954027ffc2b6a8ba63e59336d427111af3c6
GES-lbabetto <luca.babetto@greenenergystorage.eu>
2022-09-28 17:10:27 +0200

    added a while energies to compress_dftb_trajectories

--- compechem/tools/externalutilities.py
+++ compechem/tools/externalutilities.py
@@ -78,16 +78,17 @@ def compress_dftb_trajectory(filename, md_out="md.out", geo_xyz="geo_end.xyz"):
 
     with open(geo_xyz, "r") as inp:
         with open(f"{filename}.xyz", "w") as out:
-            for linenum, line in enumerate(inp):
-                if linenum == 0:
-                    atomcount = int(line)
-                if linenum % (atomcount + 2) == 0:
-                    out.write(f"{line.split()[0]}\n")
-                if linenum % (atomcount + 2) == 1:
-                    out.write(f"Step: {line.split()[2]} Energy: {energies.pop(0)} Eh\n")
-                if linenum % (atomcount + 2) > 1:
-                    out.write(
-                        f"{line.split()[0]} {round(float(line.split()[1]),3)} {round(float(line.split()[2]),3)} {round(float(line.split()[3]),3)}\n"
-                    )
+            while energies:
+                for linenum, line in enumerate(inp):
+                    if linenum == 0:
+                        atomcount = int(line)
+                    if linenum % (atomcount + 2) == 0:
+                        out.write(f"{line.split()[0]}\n")
+                    if linenum % (atomcount + 2) == 1:
+                        out.write(f"Step: {line.split()[2]} Energy: {energies.pop(0)} Eh\n")
+                    if linenum % (atomcount + 2) > 1:
+                        out.write(
+                            f"{line.split()[0]} {round(float(line.split()[1]),3)} {round(float(line.split()[2]),3)} {round(float(line.split()[3]),3)}\n"
+                        )
     logger.info(f"Compressing MD trajectory to {filename}.zip")
     os.system(f"zip {filename}.zip {filename}.xyz")
