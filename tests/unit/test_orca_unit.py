2a1fbef53b10c3d2bbe4134a3071d50ea088db8b
GES-lbabetto <luca.babetto@greenenergystorage.eu>
2022-12-21 14:36:01 +0100

    added unit test for Orca

---
+++ tests/unit/test_orca_unit.py
@@ -0,0 +1,23 @@
+import pytest
+
+from compechem.engines.orca import OrcaInput
+
+ORCADIR = "orcadir"
+
+# Test the OrcaInput class constructor
+def test_OrcaInput___init__():
+
+    try:
+        orca = OrcaInput(
+            method="HF", basis_set="def2-SVP", aux_basis="def2/J", solvent="water"
+        )
+
+    except:
+        assert False, "Unenxpected exception raised during OrcaInput class construction"
+
+    else:
+        assert orca.method == "HF"
+        assert (
+            orca.level_of_theory
+            == "OrcaInput || method: HF | basis: def2-SVP | solvent: water"
+        )
