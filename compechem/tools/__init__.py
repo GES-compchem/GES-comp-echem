cb6c8a74fa5889b83d733cba5669fe32908bf4a3
GES-ppravatto <pierpaolo.pravatto@greenenergystorage.eu>
2023-01-04 09:24:04 +0100

    Update to documentation + included vmdtools in __init__ of the tool module

--- compechem/tools/__init__.py
+++ compechem/tools/__init__.py
@@ -11,3 +11,7 @@ from compechem.tools.externalutilities import compress_dftb_trajectory
 from compechem.tools.xyz2mol import maxdist
 
 from compechem.tools.reorderenergies import reorder_energies
+
+from compechem.tools.vmdtools import render_fukui_cube
+from compechem.tools.vmdtools import render_condensed_fukui
+from compechem.tools.vmdtools import render_spin_density_cube
