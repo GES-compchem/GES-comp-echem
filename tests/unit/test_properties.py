3527f1f5f8369f5801c261ddbac9fbd8895be211
GES-ppravatto <pierpaolo.pravatto@greenenergystorage.eu>
2023-02-10 16:42:58 +0100

    Moved test_level_of_theory to integration tests to avoid failure in github workflow (unit test only)

--- tests/unit/test_properties.py
+++ tests/unit/test_properties.py
@@ -3,66 +3,7 @@ from numpy.testing import assert_array_almost_equal
 
 import compechem.config as cc
 from compechem.core.base import Engine
-from compechem.engines.orca import OrcaInput
-from compechem.engines.xtb import XtbInput
-from compechem.engines.dftbplus import DFTBInput
-from compechem.core.properties import (
-    Properties,
-    is_orca_level_of_theory,
-    is_xtb_level_of_theory,
-    is_dftb_level_of_theory,
-)
-
-
-# Test the functions to check the level of theory of the engines
-# ------------------------------------------------------------------------------------------
-
-# Test the is_orca_level_of_theory function
-def test_is_orca_level_of_theory():
-
-    engine = OrcaInput(
-        method="HF",
-        basis_set="def2-SVP",
-        aux_basis="def2/J",
-        solvent="water",
-    )
-
-    assert is_orca_level_of_theory(engine.level_of_theory) == True
-    assert is_orca_level_of_theory("dummy") == False
-
-
-# Test the is_xtb_level_of_theory function
-def test_XtbInput_is_xtb_level_of_theory():
-
-    engine = XtbInput(
-        method="gfn2",
-        solvent=None,
-    )
-
-    assert is_xtb_level_of_theory(engine.level_of_theory) == True
-    assert is_xtb_level_of_theory("dummy") == False
-
-
-# Test the is_dftb_level_of_theory function
-def test_is_dftb_level_of_theory():
-
-    engine = DFTBInput(
-        method="DFTB",
-        parameters="3ob/3ob-3-1",
-        solver=None,
-        thirdorder=True,
-        dispersion=False,
-        fermi=False,
-        fermi_temp=300.0,
-        parallel="mpi",
-        verbose=True,
-        DFTBPATH="dummy",
-        DFTBPARAMDIR="dummy",
-    )
-
-    assert is_dftb_level_of_theory(engine.level_of_theory) == True
-    assert is_dftb_level_of_theory("dummy") == False
-
+from compechem.core.properties import Properties
 
 # Test the Properties class
 # ------------------------------------------------------------------------------------------
