26067ef1b4d9852079837be1d7f673848af19fd3
GES-ppravatto <pierpaolo.pravatto@greenenergystorage.eu>
2023-03-23 16:35:46 +0100

    Added docstring to VibrationalData class

--- compechem/core/spectroscopy.py
+++ compechem/core/spectroscopy.py
@@ -1,9 +1,28 @@
 import numpy as np
-
 from typing import List, Tuple
 
 
 class VibrationalData:
+    """
+    The `VibrationalData` class holds all the information related to the vibrational properties of a given system.
+
+    Attributes
+    ----------
+    frequencies: List[float]
+        The list of frequencies associated to each mode of vibration. The leading modes, associated to rotations and
+        translations will be associated with zero frequencies.
+    normal_modes: List[np.ndarray]
+        The vectors encoding the cartesian displacements associated to each mode of vibration.
+    ir_transitions: List[Tuple[int, float]]
+        The list of tuples associated with each infrared transitions. Each tuple is composed by the index of the mode
+        involved and the corresponding intensity in km/mol.
+    ir_combination_bands: List[Tuple[int, int, float]]
+        The list of tuples associated with each combination and overtone transitions. Each tuple is composed by the
+        index of the two modes involved and the corresponding intensity in km/mol.
+    raman_transitions: List[Tuple[int, float, float]]
+        The list of tuples associated with each Raman transition. Each tuple is composed by the index of the mode
+        involved, the corresponding activity and depolarization.
+    """
 
     def __init__(self) -> None:
         self.frequencies: List[float] = []
@@ -11,3 +30,4 @@ class VibrationalData:
         self.ir_transitions: List[Tuple[int, float]] = []
         self.ir_combination_bands: List[Tuple[int, int, float]] = []
         self.raman_transitions: List[Tuple[int, float, float]] = []
+    
