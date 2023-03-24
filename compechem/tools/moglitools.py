6f0d3874390ae8b40af60f1931a37a617b7ce423
GES-ppravatto <pierpaolo.pravatto@greenenergystorage.eu>
2023-03-24 15:16:24 +0100

    Added camera option to mogli show and export

--- compechem/tools/moglitools.py
+++ compechem/tools/moglitools.py
@@ -13,9 +13,7 @@ mogli.BOND_GRAY_SHADE = 0.9
 # Define custom color maps to assign colors to the atoms based on the value
 
 
-def RdBu(
-    data: List[float], reversed: bool = False, symmetric: bool = True
-) -> List[Tuple[float]]:
+def RdBu(data: List[float], reversed: bool = False, symmetric: bool = True) -> List[Tuple[float]]:
     """
     Simple diverging colormap going from blue (low values) to red (high values).
 
@@ -142,13 +140,9 @@ class MogliViewer:
         self.__title = mol.name
         self.__width = width
         self.__height = height
-        self.__mogli_mol = mogli.Molecule(
-            mol.geometry.atomic_numbers, mol.geometry.coordinates
-        )
+        self.__mogli_mol = mogli.Molecule(mol.geometry.atomic_numbers, mol.geometry.coordinates)
 
-    def apply_coloring(
-        self, data: List[float], cmap: Callable = RdBu, kwargs: Dict[str, Any] = {}
-    ) -> None:
+    def apply_coloring(self, data: List[float], cmap: Callable = RdBu, kwargs: Dict[str, Any] = {}) -> None:
         """
         Apply a coloring to each atom in the molecule based on a list of float values.
 
@@ -168,15 +162,19 @@ class MogliViewer:
             in the given molecule.
         """
         if self.__mogli_mol.atom_count != len(data):
-            raise ValueError(
-                "Mismatch between given data and the number of atoms in the molecule"
-            )
+            raise ValueError("Mismatch between given data and the number of atoms in the molecule")
 
         self.__mogli_mol.atom_colors = cmap(data, **kwargs)
 
-    def show(self) -> None:
+    def show(self, camera: Optional[tuple] = None) -> None:
         """
         Opens an interactive windows where the molecule can be visualized.
+
+        Arguments
+        ---------
+        camera: Optional[tuple]
+            The view matrix by getting the position of the camera, the position of the center of focus and the direction
+            which should point up.
         """
         mogli.show(
             self.__mogli_mol,
@@ -184,9 +182,10 @@ class MogliViewer:
             height=self.__height,
             bonds_param=1.5,
             title=self.__title,
+            camera=camera,
         )
 
-    def export(self, path: str) -> None:
+    def export(self, path: str, camera: Optional[tuple] = None) -> None:
         """
         Saves an image of the molecule.
 
@@ -194,6 +193,9 @@ class MogliViewer:
         ---------
         path: str
             The path of the file to be saved.
+        camera: Optional[tuple]
+            The view matrix by getting the position of the camera, the position of the center of focus and the direction
+            which should point up.
         """
         mogli.export(
             self.__mogli_mol,
@@ -201,4 +203,5 @@ class MogliViewer:
             width=self.__width,
             height=self.__height,
             bonds_param=1.5,
+            camera=camera,
         )
